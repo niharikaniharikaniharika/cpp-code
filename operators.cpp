fe8e7aaafc827f5cef872962a9f5c8ccf532e3e8
niharikaniharikaniharika <niharikadhamal14@gmail.com>
2025-09-01 12:54:34 +0000

    1/9

---
+++ operators.cpp
@@ -0,0 +1,23 @@
+#include<iostream>  
+using namespace std;  
+  
+int main()  
+{  
+    int x = 5, y = 10;  
+      
+ int add = x + y;  
+ int sub = x - y;  
+ int multipli = x * y;  
+ int division = x / y;  
+ int modulus = x % y;  
+ int increment = ++x;  
+ int decrement = --x;  
+  
+ cout << "Addition: " << add << endl;  
+ cout << "Subtraction: " << sub << endl; 
+ cout << "Multiplication: " << multipli << endl;
+ cout << "Division: " << division << endl;   
+ cout << "Modulus: " << modulus << endl; 
+ cout << "Increment: " << increment <<endl;   
+ cout << "Decrement: " << decrement <<endl; 
+}  
